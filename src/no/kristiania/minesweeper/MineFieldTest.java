42d13613c0b52fa1ec698edd89fc623e975f98e5
furkan2503 <56083285+furkan2503@users.noreply.github.com>
2020-09-02 20:16:56 +0200

    Initial commit

---
+++ src/no/kristiania/minesweeper/MineFieldTest.java
@@ -0,0 +1,17 @@
+package no.kristiania.minesweeper;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MineFieldTest {
+    @Test
+    void itShowsEmptyMineField(){
+        assertArrayEquals(new String[] {"000", "000"}, displayMineField(new String[] {"...", "..."}));
+    }
+
+    private String[] displayMineField(String[] input) {
+        return new String[0];
+    }
+}
